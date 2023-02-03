c10dc9e4ae89032d0f0a5f58cd61324b80fbf789
unknown <rohangautam.it19@gmail.com>
2023-02-03 10:41:16 +0530

    firstt

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+   
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >500) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        };
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').addClass('colorr');
+        } else {
+            $('.navbar').removeClass('colorr');
+        };
+
+    });
+
+        
+})(jQuery);
+
