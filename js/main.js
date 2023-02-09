e33bfb6b26f0b32dc03717697a50b8d2a64b55bc
unknown <rohangautam.it19@gmail.com>
2023-02-09 11:26:30 +0530

    discover-div

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@
 
    
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() >500) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        };
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').addClass('colorr');
-        } else {
-            $('.navbar').removeClass('colorr');
-        };
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() >500) {
+    //         $('.navbar').addClass('sticky-top shadow-sm');
+    //     } else {
+    //         $('.navbar').removeClass('sticky-top shadow-sm');
+    //     };
+    //     if ($(this).scrollTop() > 200) {
+    //         $('.navbar').addClass('colorr');
+    //     } else {
+    //         $('.navbar').removeClass('colorr');
+    //     };
 
-    });
+    // });
 
         
 })(jQuery);
